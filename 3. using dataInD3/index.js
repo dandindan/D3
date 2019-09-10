e913c48da769a11994a1225d18c1ce032fa9e707
Dan Din <431bog431@gmail.com>
2019-09-10 21:27:19 +0300

    cont arroe function

--- 3. using dataInD3/index.js
+++ 3. using dataInD3/index.js
@@ -1,15 +1,14 @@
-const data = [ {width:200, height:200, fill:"purple"}];
+const data = [{
+    width: 200,
+    height: 200,
+    fill: "purple"
+}];
 
 const svg = d3.select('svg');
 
 svg.select('rect')
-.data(data)
-.attr('width', (d,i,n) => {
-    console.log(n[i])
-    //the n[i] replace the this in arrow function
-    return d.width})
-.attr('height', function(d){
-        console.log(this)
-       // can use the this keyword in a regular function!
-    return d.height})
-.attr('fill', function(d){return d.fill});
+    .data(data)
+    .attr('width', (d, i, n) => d.width)
+    // if its only one parameter no need for parentheses!!
+    .attr('height', d => d.height)
+    .attr('fill', d => d.fill);
