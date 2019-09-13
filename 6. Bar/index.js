d38c65b7da17c2aacb112f2fd57eee059098eb19
Dan Din <431bog431@gmail.com>
2019-09-13 13:19:46 +0300

    add Band Scale

--- 6. Bar/index.js
+++ 6. Bar/index.js
@@ -1,28 +1,35 @@
- 
-const svg = d3.select('svg');
+ const svg = d3.select('svg');
 
-d3.json('menu.json').then(data=>{
+ d3.json('menu.json').then(data => {
 
-    const y = d3.scaleLinear()
-        .domain([0,1000])
-        .range([0,500]);
-    // join the data to he rects
-    const rects = svg.selectAll('rect')
-    .data(data)
+     const y = d3.scaleLinear()
+         .domain([0, 1000])
+         .range([0, 500]);
+
+     const x = d3.scaleBand()
+         .domain(data.map(item => item.name))
+         .range([0, 500])
+         .paddingInner(0.2)
+         .paddingOuter(0.2);
 
 
-    
-    rects.attr('width', 50)
-    .attr('height', d => y(d.order))
-    .attr('fill',"orange")
-    .attr('x',(d,i)=> i*70);
+     // join the data to he rects
+     const rects = svg.selectAll('rect')
+         .data(data)
 
-    // append enter to the DOM
-    rects.enter()
-    .append('rect')
-    .attr('width', 50)
-    .attr('height', d => y(d.order))
-    .attr('fill',"orange")
-    .attr('x',(d,i)=> i*70);
 
-})
+
+     rects.attr('width', x.bandwidth)
+         .attr('height', d => y(d.order))
+         .attr('fill', "orange")
+         .attr('x', d => x(d.name));
+
+     // append enter to the DOM
+     rects.enter()
+         .append('rect')
+         .attr('width', x.bandwidth)
+         .attr('height', d => y(d.order))
+         .attr('fill', "orange")
+         .attr('x', d => x(d.name));
+
+ })
