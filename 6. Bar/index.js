9abf5f29a6bc4f3ba29a1d5013b03a06466d4472
Dan Din <431bog431@gmail.com>
2019-09-13 12:59:18 +0300

    scale the y value

--- 6. Bar/index.js
+++ 6. Bar/index.js
@@ -2,6 +2,10 @@
 const svg = d3.select('svg');
 
 d3.json('menu.json').then(data=>{
+
+    const y = d3.scaleLinear()
+        .domain([0,1000])
+        .range([0,500]);
     // join the data to he rects
     const rects = svg.selectAll('rect')
     .data(data)
@@ -9,7 +13,7 @@ d3.json('menu.json').then(data=>{
 
     
     rects.attr('width', 50)
-    .attr('height', d => d.order)
+    .attr('height', d => y(d.order))
     .attr('fill',"orange")
     .attr('x',(d,i)=> i*70);
 
@@ -17,7 +21,7 @@ d3.json('menu.json').then(data=>{
     rects.enter()
     .append('rect')
     .attr('width', 50)
-    .attr('height', d => d.order)
+    .attr('height', d => y(d.order))
     .attr('fill',"orange")
     .attr('x',(d,i)=> i*70);
 
