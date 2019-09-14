492e1dd8b36097cae03c7cb4fc8b93b30423427a
Dan Din <431bog431@gmail.com>
2019-09-14 13:06:14 +0300

    scale and inverting the graph

--- 6. Bar/index.js
+++ 6. Bar/index.js
@@ -1,50 +1,69 @@
- const svg = d3.select('.canvas')
-     .append('svg')
-     .attr('width', 600)
-     .attr('height', 600);
-    
- const margin = {top:20, right:20, bottom:100, left:100};
- const graphWidth = 600- margin.left - margin.right;
- const graphHeight = 600 - margin.top - margin.bottom;
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+const margin = {
+    top: 20,
+    right: 20,
+    bottom: 100,
+    left: 100
+};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
     .attr('width', graphWidth)
     .attr('height', graphHeight)
-    .attr('transform', `translate(${margin.left},${margin.top})`)
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
- d3.json('menu.json').then(data => {
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`);
 
-     const y = d3.scaleLinear()
-         .domain([0, 1000])
-         .range([0, 500]);
-
-     const x = d3.scaleBand()
-         .domain(data.map(item => item.name))
-         .range([0, 500])
-         .paddingInner(0.2)
-         .paddingOuter(0.2);
-
-     const min = d3.min(data, d => d.order);
-     const max = d3.max(data, d => d.order);
-     const extent = d3.extent(data, d => d.order);
-
-     // join the data to he rects
-     const rects = graph.selectAll('rect')
-         .data(data)
+const yAxisGroup = graph.append('g');
 
 
+d3.json('menu.json').then(data => {
 
-     rects.attr('width', x.bandwidth)
-         .attr('height', d => y(d.order))
-         .attr('fill', "orange")
-         .attr('x', d => x(d.name));
+    const y = d3.scaleLinear()
+        .domain([0, 1000])
+        .range([graphHeight, 0]);
 
-     // append enter to the DOM
-     rects.enter()
-         .append('rect')
-         .attr('width', x.bandwidth)
-         .attr('height', d => y(d.order))
-         .attr('fill', "orange")
-         .attr('x', d => x(d.name));
+    const x = d3.scaleBand()
+        .domain(data.map(item => item.name))
+        .range([0, 500])
+        .paddingInner(0.2)
+        .paddingOuter(0.2);
 
- })
+    const min = d3.min(data, d => d.order);
+    const max = d3.max(data, d => d.order);
+    const extent = d3.extent(data, d => d.order);
+
+    // join the data to he rects
+    const rects = graph.selectAll('rect')
+        .data(data)
+
+
+
+    rects.attr('width', x.bandwidth)
+        .attr('height', d => graphHeight - y(d.order))
+        .attr('fill', "orange")
+        .attr('y', d => y(d.order));
+
+
+    // append enter to the DOM
+    rects.enter()
+        .append('rect')
+        .attr('width', x.bandwidth)
+        .attr('height', d => graphHeight - y(d.order))
+        .attr('fill', "orange")
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.order));
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+})
