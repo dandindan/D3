20125b8d30ac195181569f8e0767e6dc7c442d9a
Dan Din <431bog431@gmail.com>
2020-04-20 23:36:40 +0300

    add files

--- .D3.js in action/1/app.js
+++ .D3.js in action/1/app.js
@@ -1,3 +1,7 @@
 d3.select("#someDiv").style("border", "5px darkgray dashed");
 d3.select("#someDiv").attr("id", "newID");
 d3.select("#someCheckbox").property("checked", true);
+
+// put in the console  d3.select("div").style("background", "lightblue").style("border", "solid
+// black 1px").html("You have now dynamically changed the content of a web page
+// element");
---
+++ .D3.js in action/1/app1.js
@@ -0,0 +1,2 @@
+d3.select('circle').remove();
+d3.select("rect").style("fill", "purple")
